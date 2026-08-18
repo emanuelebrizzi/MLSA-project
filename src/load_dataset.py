0b1280f0ddc84b4401a9c405cad4e2eaa0142ccd
brizziemanuele <brizziemanuele@gmail.com>
2026-08-18 14:03:44 +0200

    Create load_dataset.py
    Load Python portion of the CodeSearchNet dataset

---
+++ src/load_dataset.py
@@ -0,0 +1,21 @@
+from datasets import load_dataset
+
+
+DATASET_NAME = "code_search_net"
+LANGUAGE = "python"
+
+
+def load_codesearchnet():
+    """
+    Load the Python portion of the CodeSearchNet dataset.
+    """
+    dataset = load_dataset(DATASET_NAME, LANGUAGE)
+    return dataset
+
+
+def main():
+    dataset = load_codesearchnet()
+
+
+if __name__ == "__main__":
+    main()
